3ccfda2aeeb3541141aee8fdc2c9e0bccb07121d
shekar <rajksreddy@gmail.com>
2024-07-02 22:27:14 +0530

    header added

---
+++ config.js
@@ -0,0 +1,25 @@
+
+const config = {
+  // REQUIRED
+  appName: "Nextdir",
+  // REQUIRED: a short description of your app for SEO tags (can be overwritten)
+  appDescription:
+    "The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web app.",
+  // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
+  domainName: "shipfa.st",
+  crisp: {
+    // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
+    id: "",
+    // Hide Crisp by default, except on route "/". Crisp is toggled with <ButtonSupport/>. If you want to show Crisp on every routes, just remove this below
+    onlyShowOnRoutes: ["/"],
+  },
+ 
+  auth: {
+    // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
+    loginUrl: "/api/auth/signin",
+    // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in apiClient (/libs/api.js) upon 401 errors from our API & in ButtonSignin.js
+    callbackUrl: "/dashboard",
+  },
+};
+
+export default config;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+          // NextJS <Image> component needs to whitelist domains for src={}
+          "lh3.googleusercontent.com",
+          "pbs.twimg.com",
+          "images.unsplash.com",
+          "logos-world.net",
+        ],
+      },
+};
 
 export default nextConfig;
+
